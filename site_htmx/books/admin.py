6113b5f414b904f665539953ceb44941cda4461c
Michael Florya <mih2303@mail.ru>
2024-04-05 00:05:20 +0300

    create model Book

--- site_htmx/books/admin.py
+++ site_htmx/books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "price", "read")
+
+
+admin.site.register(Book, BookAdmin)
--- site_htmx/books/models.py
+++ site_htmx/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.PositiveIntegerField(default=0)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
